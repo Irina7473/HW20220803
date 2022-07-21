e98c8f5097e1277d14a28e21a1d96714537652e1
Irina <ten7473@mail.ru>
2022-07-21 18:14:05 +0500

    Задания 1,2,3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        //Задание 1
+        System.out.println("“Your time is limited,\n" +
+                "\t so don’t waste it \n" +
+                " \t\t living someone else’s life”\n" +
+                " \t\t\t Steve Jobs");
+
+        //Задание 2
+        System.out.println("Введите число");
+        int number = in.nextInt();
+        System.out.println("Введите процент, который нужно посчитать, в виде целого числа");
+        int percent = in.nextInt();
+        System.out.println("Введенный процент от введенного числа равен " + number * percent / 100);
+
+        //Задание 3
+        int a, b, c;
+        System.out.println("Введите 3 цифры через пробел");
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        System.out.println("Из введенных цифр получаем число " + a + b + c);
+
+        //Задание 4
     }
 }
