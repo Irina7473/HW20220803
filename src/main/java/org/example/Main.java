0fa99db95c937af44c44f120c158fcbac55aeb6d
Irina <ten7473@mail.ru>
2022-07-24 21:07:55 +0500

    Задание 6,7,8,9,10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
-
 import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -38,7 +39,6 @@ public class Main {
         Пользователь вводит шестизначное число. Необходимо поменять в этом числе первую и шестую цифры,
         а также  вторую и пятую цифры.  Если пользователь ввел не шестизначное число
         требуется вывести сообщение об ошибке*/
-
         System.out.println("Введите 6-тизначное число  ");
         String str = in.nextLine();
         str = str.trim();
@@ -72,7 +72,6 @@ public class Main {
         Пользователь вводит с клавиатуры номер месяца (от 1 до 12). В зависимости от полученного номера месяца
         программа выводит на экран надпись: Winter, Spring, Summer, Autumn.
         Если пользователь ввел значение не в диапазоне от 1 до 12 требуется вывести сообщение об ошибке*/
-
         boolean ok=false;
         System.out.print("Введите номер месяца в виде целого числа от 1 до 12 -  ");
 
@@ -88,5 +87,114 @@ public class Main {
                 ok=true;
             }
         }
+
+
+        /*Задание 6
+        Пользователь вводит с клавиатуры количество метров. В зависимости от выбора пользователя
+        программа переводит метры в мили, дюймы или ярды.*/
+
+        System.out.print("Введите количество метров для конвертации -  ");
+        int metr = in.nextInt();
+        System.out.print("Введите единицу измерения для конвертации метров, 1 - мили, 2 - дюймы, 3 - ярды :  ");
+        int unitLength = in.nextInt();
+        switch(unitLength){
+
+            case 1:
+                System.out.println(metr + " метров = " + metr * 0.00062137 + " милей");
+                break;
+            case 2:
+                System.out.println(metr + " метров = " + metr * 39.37 + " дюймов");
+                break;
+            case 3:
+                System.out.println(metr + " метров = " + metr * 1.0936 + " ярдов");
+                break;
+            default:
+                System.out.println("Неизвестная единица длины");
+                return;
+        }
+
+
+        /*Задание 7
+        Пользователь вводит с клавиатуры два числа. Нужно показать все нечетные числа в указанном диапазоне. 
+        Если границы диапазона указаны неправильно требуется произвести нормализацию границ. */
+        int num1, num2;
+        System.out.print("Введите через пробел 2 целых числа, границы диапазона :  ");
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        int min = num1 < num2 ? num1 : num2;
+        int max = num1 > num2 ? num1 : num2;
+        System.out.print("Нечетные числа в указанном диапазоне :  ");
+        for (int i = min; i<=max; i++)
+            if ( i %2 != 0) System.out.print(i + ", ");
+
+
+        /*Задание 8
+        Показать на экран таблицу умножения в диапазоне, в указанном пользователем. */
+        System.out.print("Введите через пробел 2 целых числа, границы диапазона :  ");
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        min = num1 < num2 ? num1 : num2;
+        max = num1 > num2 ? num1 : num2;
+        System.out.println("Таблица умножения для введенного диапазона");
+        for (int i = min; i<=max; i++){
+            for (int j = 1; j<=10; j++)
+                System.out.print(i + "*" + j + "=" + i*j + " ");
+            System.out.println();
+            System.out.println("-----------------------------------------------");
+        }
+
+        /*Задание 9
+        В одномерном массиве, заполненном случайными числами, определить минимальный и максимальный элементы,
+        посчитать количество отрицательных и положительных элементов, посчитать количество нулей. */
+        int [] nums = new int[10];
+        Random rand = new Random();
+        int pos=0, neg=0, zero=0, even=0, odd=0;
+        for (int i =0; i<nums.length; i++) {
+            nums[i] = -3 + rand.nextInt(7);
+            System.out.print(nums[i] + " ");
+            if(nums[i]==0) zero++;
+            if(nums[i]>0) pos++;
+            if(nums[i]<0) neg++;
+            if(nums[i]%2 !=0) odd++;
+                else if(nums[i]!=0) even++;
+        }
+        System.out.println();
+        System.out.println("В сгенерированном массиве " +pos+ " положительных, " +neg+ " отрицательных, " +zero+ " нулевых, " +even+ " четных, " +odd+ " нечетных элементов");
+
+
+        /*Задание 10
+        Есть одномерный массив, заполненный случайными числами. На основании данных этого массива нужно:
+        ■ Создать одномерный массив, содержащий только четные числа из первого массива;
+        ■ Создать одномерный массив, содержащий только нечетные числа из первого массива;
+        ■ Создать одномерный массив, содержащий только отрицательные числа из первого массива;
+        ■ Создать одномерный массив, содержащий только положительные числа из первого массива. */
+        int [] arrEven = new int[even];
+        int [] arrOdd = new int[odd];
+        int [] arrNeg = new int[neg];
+        int [] arrPos = new int[pos];
+        int e=0, o=0, n=0, p=0;
+        for (int i =0; i<nums.length; i++) {
+            if(nums[i]%2 !=0){
+                arrOdd[o] = nums[i];
+                o++;
+            }
+            else if(nums[i]!=0) {
+                arrEven[e] = nums[i];
+                e++;
+            }
+            if(nums[i]<0) {
+                arrNeg[n] = nums[i];
+                n++;
+            }
+            if(nums[i]>0) {
+                arrPos[p] = nums[i];
+                p++;
+            }
+        }
+        System.out.println(Arrays.toString(arrEven));
+        System.out.println(Arrays.toString(arrOdd));
+        System.out.println(Arrays.toString(arrNeg));
+        System.out.println(Arrays.toString(arrPos));
+
     }
 }
